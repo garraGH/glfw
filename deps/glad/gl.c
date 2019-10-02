950c16de504f0abb6e18875602c0d0d945b2b34b
garra <garra_gwc@126.com>
2019-10-02 16:25:51 +0800

    add gl.c

---
+++ deps/glad/gl.c
@@ -0,0 +1,14 @@
+/*============================================
+* Copyright(C)2019 Garra. All rights reserved.
+* 
+* file        : /home/garra/study/dnn/deps/modules/glfw/deps/glad/gl.c
+* author      : Garra
+* time        : 2019-10-02 15:05:52
+* description : 
+*
+============================================*/
+
+
+#include "gl.h"
+
+
